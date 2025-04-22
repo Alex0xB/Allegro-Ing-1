86126d5850a1bd40bab6620fb8daaee65d701f46
Mikaelmalavaux <mikael.malavaux@edu.ece.fr>
2025-04-22 18:11:14 +0200

    test mika3

--- personnage.c
+++ personnage.c
@@ -1,5 +1,3 @@
-//
-// Created by mikae on 22/04/2025.
-//
-
 #include "personnage.h"
+#include <allegro.h>
+
--- personnage.h
+++ personnage.h
@@ -1,8 +1,19 @@
-//
-// Created by mikae on 22/04/2025.
-//
-
 #ifndef PERSONNAGE_H
 #define PERSONNAGE_H
+#include <allegro.h>
+
+typedef struct {
+    float x, y;              // Position du personnage
+    float vy;                // Vitesse verticale (vol / chute)
+    float lift;              // Force vers le haut quand on appuie sur espace
+    int width, height;       // Dimensions du sprite
+
+    int isAlive;             // 1 = vivant, 0 = mort
+    int spriteIndex;         // Frame actuelle de l’animation
+    int spriteCount;         // Nombre de sprites utilisés
+    int animationTimer;      // Pour gérer l’animation
+
+    BITMAP *sprites[10];     // Tableau simple de sprites (max 10)
+} Personnage;
 
 #endif //PERSONNAGE_H
