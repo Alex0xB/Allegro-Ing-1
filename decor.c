d0ea8358f352e8808469b831f47bde4e08617f43
Remydoan <remy.doan@edu.ece.fr>
2025-04-25 19:22:23 +0200

    decor1

---
+++ decor.c
@@ -0,0 +1,27 @@
+#include <allegro.h>
+#include "decor.h"
+
+static BITMAP *decor = NULL;
+
+#define FICHIER_DECOR "decor1.bmp"
+
+void charger_decor() {
+    decor = load_bitmap(FICHIER_DECOR, NULL);
+    if (!decor) {
+        allegro_message("Erreur : impossible de charger le décor !");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void afficher_decor() {
+    if (decor) {
+        blit(decor, screen, 0, 0, 0, 0, decor->w, decor->h);
+    }
+}
+
+void detruire_decor() {
+    if (decor) {
+        destroy_bitmap(decor);
+        decor = NULL;
+    }
+}
---
+++ decor.h
@@ -0,0 +1,12 @@
+#ifndef DECOR_H
+#define DECOR_H
+
+#include <allegro.h>
+
+void charger_decor();
+
+void afficher_decor();
+
+void detruire_decor();
+
+#endif
