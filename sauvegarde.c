cb240438a70117f60c5cd4f014580558005d1d6c
Alex0xB <alexis.bihour@edu.ece.fr>
2025-05-17 15:55:24 +0200

    Chargement et sauvegarde du joueur fini manque plus que l'affichage dans Allegro

--- sauvegarde.c
+++ sauvegarde.c
@@ -5,54 +5,27 @@
 #include <string.h>
 
 void sauvegarder(t_personnage* personnage) {
-    FILE* liste_nom = fopen("nom.txt", "r"); //On verifie si le joueur existe deja
-    bool nom_existant = false;
-    char nom_lu[100];
     char nom_txt[100];
-    while(fscanf(liste_nom, "%s", nom_lu) == 1) {
-        if(strcmp(personnage->nom, nom_lu) == 0) {
-            nom_existant = true;
-            break;
-        }
-    }
-    fclose(liste_nom);
     strcpy(nom_txt, personnage->nom);
     strcat(nom_txt, ".txt");
-    if(nom_existant) {
-        FILE* pt = fopen(nom_txt, "w");
-        if (pt == NULL) {
-            printf("Erreur d'ouvrir le fichier\n");
-        }
-        fprintf(pt, "%s\n", personnage->nom);
-        fprintf(pt, "%d\n", personnage->nb_mort);
-        fprintf(pt, "%d\n", personnage->nb_niveau);
-        fclose(pt);
-    }
-    else {
-        char nom_fichier_test[100];
-        strcmp(nom_fichier_test, personnage->nom);
-        strcat(nom_fichier_test, ".txt");
-        FILE* pt2 = fopen(nom_fichier_test, "w"); //Je cree un nouveau fichier
-        if (pt2 == NULL) {
-            printf("Erreur d'ouvrir le fichier\n");
-        }
-        fprintf(pt2, "%s\n", personnage->nom);
-        fprintf(pt2, "%d\n", personnage->nb_mort);
-        fprintf(pt2, "%d\n", personnage->nb_niveau);
-        fclose(pt2);
 
-        FILE* pt3 = fopen("nom.txt", "a"); //Je rajoute le nom du joueur dans la liste
-        fprintf(pt3, "%s\n", personnage->nom);
-        fclose(pt3);
-    }
+    FILE* pt = fopen(nom_txt, "w"); //on ouvre notre ficher avec le nom du personnage avec un .txt
+
+    fprintf(pt, "%s\n", personnage->nom);
+    fprintf(pt, "%d\n", personnage->nb_mort);
+    fprintf(pt, "%d\n", personnage->nb_niveau);
+    fprintf(pt, "%d\n", personnage->niveau1_fini);
+    fprintf(pt, "%d\n", personnage->niveau2_fini);
+    fprintf(pt, "%d\n", personnage->niveau3_fini);
+    fclose(pt);
 }
 
 t_personnage* charger(bool **peux_jouer) {// On fait charger le joueur si il y a une sauvegarde a charger sinon faire directement initialiser joueur
     t_personnage* personnage;
     char nom[100][100];
-    FILE* pt= fopen("nom.txt", "r");
 
     //On recupere tous nos nom
+    FILE* pt= fopen("nom.txt", "r");
     int j = 0;
     while(fscanf(pt, "%s", nom[j]) == 1) {
         j++;
@@ -60,24 +33,49 @@ t_personnage* charger(bool **peux_jouer) {// On fait charger le joueur si il y a
     fclose(pt);
 
     //Afficher tous les nom (Code avec menu Allegro a integre ici)
-    char nom2[100];
+    char nom2[100]; //=>correspond au pseudo qu'on recupere
+    char nom2_txt[100]; //=>on lui rajoute .txt
     while(peux_jouer == false) {
         **peux_jouer = true;
     }
 
+    //on  rajoute .txt a la fin de nom2[100] pour pouvoir lire le fichier
+    strcpy(nom2_txt, nom2);
+    strcat(nom2_txt, ".txt");
 
-    //On recupere les informations
+    //On sauvegarde le nom si il est nouveau dans notre fichier nom.txt
+    int compteur = 0;
+    for(int i = 0; i < j; i++) {
+        if(strcmp(nom[i], nom2) != 0) {//si le nom ne correspond pas on incremente de 1 le compteur
+            compteur++;
+        }
+    }
+    if(compteur == j) { //Si mon compteur vaut le nombre de pseudo enregistrer cela signifie que le nom n'existe pas
+        FILE* pt= fopen("nom.txt", "r");
+        fprintf(pt, "%s\n", nom2);//On ne sauvegarde que des noms et pas des noms avec des .txt a la fin
+        fclose(pt);
+    }
 
-    FILE* pt2 = fopen(nom2, "r");
+    //On recupere les informations si le compte existe sinon on lui en cree
+    if(compteur == j) {
+        FILE* pt2 = fopen(nom2_txt, "r"); //J'ouvre mon fichier .txt
 
-    fscanf(pt2, "%s", personnage->nom);
-    fscanf(pt2, "%d", personnage->nb_mort);
-    fscanf(pt2, "%d", personnage->nb_niveau);
-    fscanf(pt2, "%d", personnage->niveau1_fini);
-    fscanf(pt2, "%d", personnage->niveau2_fini);
-    fscanf(pt2, "%d", personnage->niveau3_fini);
-    fclose(pt2);
-
+        fscanf(pt2, "%s", personnage->nom);
+        fscanf(pt2, "%d", personnage->nb_mort);
+        fscanf(pt2, "%d", personnage->nb_niveau);
+        fscanf(pt2, "%d", personnage->niveau1_fini);
+        fscanf(pt2, "%d", personnage->niveau2_fini);
+        fscanf(pt2, "%d", personnage->niveau3_fini);
+        fclose(pt2);
+    }
+    else {
+        strcmp(personnage->nom, nom2);
+        personnage->nb_mort = 0;
+        personnage->nb_niveau = 0;
+        personnage->niveau1_fini = 0;
+        personnage->niveau2_fini = 0;
+        personnage->niveau3_fini = 0;
+    }
     return personnage;
 }
 
